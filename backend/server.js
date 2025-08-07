630d5863775811368a7e30649b1be4223da74e87
elIgnorante <alvarojzg2@gmail.com>
2025-08-07 16:45:11 -0600

    Configuracion para despliege

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js'; 
 import productsRoutes from './routes/products.route.js';
@@ -17,6 +18,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Resolviendo el directorio actual para servir archivos estáticos correctamente
+// Esto es parte de la configuración para servir el frontend en producción
+const __dirname = path.resolve();
+
 app.use(express.json({limit: "10mb"})); // Middleware para parsear JSON
 app.use(cookieParser()); // Middleware para parsear cookies
 
@@ -26,6 +31,19 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+// Middleware para servir archivos estáticos en producción
+// Esto permite que el frontend construido con React se sirva correctamente al cliente
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    
+    // Esto redirige todas las solicitudes al archivo index.html del frontend
+    // Esto es necesario para que las rutas del frontend funcionen correctamente
+    // Para de esta forma permitir el enrutamiento del lado del cliente
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
