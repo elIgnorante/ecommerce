f80d28a8b9125a2779ea970fa79e54c37a006679
elIgnorante <alvarojzg2@gmail.com>
2025-08-07 18:25:30 -0600

    Correccion

--- backend/server.js
+++ backend/server.js
@@ -35,14 +35,13 @@ app.use("/api/analytics", analyticsRoutes);
 // Middleware para servir archivos estáticos en producción
 // Esto permite que el frontend construido con React se sirva correctamente al cliente
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    
-    // Esto redirige todas las solicitudes al archivo index.html del frontend
-    // Esto es necesario para que las rutas del frontend funcionen correctamente
-    // Para de esta forma permitir el enrutamiento del lado del cliente
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
+    // Sirve archivos estáticos del frontend
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+  
+  // Maneja todas las demás rutas
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist', 'index.html'));
+  });
 }
 
 app.listen(PORT, () => {
